c1ef890ced3c2c037846fb9aa5235139f7820cda
TronFlyn <ai974088741@outlook.com>
2019-04-19 09:43:05 +0800

    first commit

---
+++ aosp_sagit.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product-if-exists, vendor/apps/MiuiCamera/config.mk)
+$(call inherit-product-if-exists, vendor/apps/GooglePinYin/config.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common AospExtended stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# OTA
+$(call inherit-product, vendor/aosp/config/ota.mk)
+
+# Inherit from sagit device
+$(call inherit-product, device/xiaomi/sagit/device.mk)
+
+PRODUCT_NAME := aosp_sagit
+PRODUCT_DEVICE := sagit
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 6
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sagit-user 8.0.0 OPR1.170623.027 V9.2.3.0.OCAMIEK release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/sagit/sagit:8.0.0/OPR1.170623.027/V9.2.3.0.OCAMIEK:user/release-keys
